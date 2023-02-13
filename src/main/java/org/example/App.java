e82b23c514e4f62c479d40fb6099ea931921e18d
Rohit-Rajput21 <rohit232425rr@gmail.com>
2023-02-13 22:12:14 +0530

    commit: Image saving in DB & Annotation

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,14 +5,18 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Date;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main ( String[] args ) throws IOException {
 
         System.out.println( "Project started......" );
         SessionFactory factory=new Configuration().configure().buildSessionFactory();
@@ -26,9 +30,23 @@ public class App
         student.setCity("Indore");
         System.out.println(student);
 
+        Address ad=new Address();
+        ad.setStreet("street_1");
+        ad.setCity("Indore");
+        ad.setOpen(true);
+        ad.setAddedDate(new Date());
+        ad.setX(1234.345);
+
+        FileInputStream fis=new FileInputStream("src/main/resources/tree-736885__480.jpg");
+        byte[] data=new byte[fis.available()];
+        fis.read(data);
+        ad.setImage(data);
+
+
         Session session=factory.openSession();
         Transaction transaction=session.beginTransaction();
         session.save(student);
+        session.save(ad);
         transaction.commit();
         session.close();
     }
